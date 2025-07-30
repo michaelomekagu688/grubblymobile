6825b95f58a8cca0865b7436b34ad844f003c9a9
Omekagu Michael <michaelomekagu@gmail.com>
2025-07-30 20:28:13 +0100

    modified layout.jsx

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Nunito, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -19,12 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <style>
-          @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lavishly+Yours&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');
-        </style>
-      </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`nunito`}>
         {children}
       </body>
     </html>
